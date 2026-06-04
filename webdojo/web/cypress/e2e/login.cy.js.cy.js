adeff8c06fff59343c2ffab907b607105808f833
Felipe Lundgren <felipec5l2@hotmail.com>
2026-06-04 17:12:15 -0300

    feat: Add Login Test

--- webdojo/web/cypress/e2e/login.cy.js.cy.js
+++ webdojo/web/cypress/e2e/login.cy.js.cy.js
@@ -1,6 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe("login",() => {
+  it("Deve logar com sucesso",() =>{
     cy.viewport(1920, 1080)
     cy.visit('http://localhost:3000')
+    cy.get('#email').type("papito@webdojo.com")
+    cy.get('#password').type("katana123")
+    cy.contains('button', 'Entrar').click()
+    cy.wait(2000)
+
+
+    cy.get('[data-cy="user-name"]')
+    .should("be.visible")
+    .and("contain", "Fernando Papito")
+
+    cy.get('[data-cy="welcome-message"]')
+    .should("be.visible")
+    .and("contain", "Olá QA, esse é o seu Dojo para aprender Automação de Testes.")
+
   })
 })
