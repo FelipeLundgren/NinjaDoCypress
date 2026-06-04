8ac9e33b58496105f2cd04da9cc347c2ab8bd79d
Felipe Lundgren <felipec5l2@hotmail.com>
2026-06-04 17:38:22 -0300

    fix: refactor code

--- webdojo/web/cypress/e2e/login.cy.js.cy.js
+++ webdojo/web/cypress/e2e/login.cy.js.cy.js
@@ -1,13 +1,8 @@
 describe("login",() => {
   it("Deve logar com sucesso",() =>{
-    cy.viewport(1920, 1080)
-    cy.visit('http://localhost:3000')
-    cy.get('#email').type("papito@webdojo.com")
-    cy.get('#password').type("katana123")
-    cy.contains('button', 'Entrar').click()
-    cy.wait(2000)
-
-
+    cy.start()
+    cy.submitLogin("papito@webdojo.com","katana123")
+    
     cy.get('[data-cy="user-name"]')
     .should("be.visible")
     .and("contain", "Fernando Papito")
@@ -15,38 +10,19 @@ describe("login",() => {
     cy.get('[data-cy="welcome-message"]')
     .should("be.visible")
     .and("contain", "Olá QA, esse é o seu Dojo para aprender Automação de Testes.")
-
-
-
   })
   it("Não deve logar com senha invalida",() =>{
-    cy.viewport(1920, 1080)
-    cy.visit('http://localhost:3000')
-    cy.get('#email').type("papito@webdojo.com")
-    cy.get('#password').type("katana321")
-    cy.contains('button', 'Entrar').click()
-    cy.wait(2000)
+    cy.start()
+    cy.submitLogin("papito@webdojo.com","katana321")
 
     cy.contains("Acesso negado! Tente novamente.")
       .should("be.visible")
-
-    
-    
-
   })
   it("Não deve logar com email invalido",() =>{
-    cy.viewport(1920, 1080)
-    cy.visit('http://localhost:3000')
-    cy.get('#email').type("papita@webdojo.com")
-    cy.get('#password').type("katana123")
-    cy.contains('button', 'Entrar').click()
-    cy.wait(2000)
+    cy.start()
+    cy.submitLogin("papita@webdojo.com","katana123")
 
     cy.contains("Acesso negado! Tente novamente.")
       .should("be.visible")
-
-    
-    
-
   })
 })
