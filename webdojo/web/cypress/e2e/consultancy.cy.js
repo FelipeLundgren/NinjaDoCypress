2cfb124afe86481a7807b31f8ae7a6b55d89b68a
Felipe Lundgren <felipec5l2@hotmail.com>
2026-06-05 16:51:34 -0300

    feat: finish E2E consultancy tests

---
+++ webdojo/web/cypress/e2e/consultancy.cy.js
@@ -0,0 +1,72 @@
+describe("Formulario de Consultoria",() => {
+    it("Deve solicitar consultoria individual",() =>{
+        cy.start()
+        cy.submitLogin("papito@webdojo.com", "katana123")
+
+        cy.goTo("Formulários", "Consultoria")
+
+        cy.get('#name').type("Felipe Lundgren")
+
+        cy.get('input[placeholder="Digite seu email"]').type('felipe@webdojo.com')
+
+        cy.get('input[placeholder="(00) 00000-0000"]').type('11 98765-4321')
+            .should("have.value", "(11) 98765-4321")
+
+        cy.get('#consultancyType').select("In Company")
+
+        cy.contains("label", "Pessoa Física")
+            .find('input')
+            .click()
+            .should("be.checked")
+
+        cy.contains("label", "Pessoa Jurídica")
+            .find('input')
+            .should("be.not.checked")
+
+        cy.contains("label", "CPF")
+            .parent()
+            .find('input')
+            .type('12345678900')
+            .should("have.value", "123.456.789-00")
+
+        
+        
+        const discoveryChannels = 
+        ["Instagram", "LinkedIn", "Udemy", "YouTube", "Indicação de Amigo"]
+        
+        discoveryChannels.forEach(channel => {
+            cy.contains("label", channel)
+                .find('input')
+                .check()
+                .should("be.checked")
+        })
+
+        cy.get('input[type="file"]').selectFile("./cypress/fixtures/Document.pdf", { force: true })
+
+        cy.get('textarea[placeholder="Descreva mais detalhes sobre sua necessidade"]')
+            .type("Gostaria de uma consultoria para minha empresa, focada em automação de testes.")
+
+        const techs = ["Cypress", "Selenium", "Playwright"]
+        
+        techs.forEach(tech => {
+            cy.get('#technologies')
+            .type(tech + "{enter}")
+
+        cy.contains('label','Tecnologias')
+            .parent()
+            .contains('span', tech)
+            .should("be.visible")})
+
+        cy.contains('label','termos de uso' )
+            .find('input')
+            .check()
+            .should("be.checked")
+
+        cy.contains('button', 'Enviar formulário').click()
+
+        cy.contains("Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.")
+            .should("be.visible")
+            
+    })
+})
+
